31ef2ed7ac1c58c397cff982fba7dd405fa9e22f
Green Gem <chris_waitt@me.com>
2023-09-30 11:04:04 +0100

    Saving routines now works but i have hardcoded the user id temporarily, it seems next auth and app router will need special attention

--- app/api/routines/save-routine/route.js
+++ app/api/routines/save-routine/route.js
@@ -1,39 +1,43 @@
-import { getSession } from 'next-auth/react';
-import { PrismaClient } from '@prisma/client';
+import prisma from '../../../../utils/prisma';
 
-const prisma = new PrismaClient();
+export async function POST(request) {
+  // Read the request body as text
+  const rawData = await request.text();
+  console.log("Raw request body:", rawData);
 
-export async function POST(req, res) {
-    // Get user session
-    const session = await getSession({ req });
+  // Parse the body data as JSON
+  const data = JSON.parse(rawData);
 
-    if (!session) {
-        return res.status(401).json({ success: false, message: "Not authenticated" });
-    }
+  const userId = "cln4dplo80000gwz1joipss3e"; // Use the specific user ID
+  const { routineName, exercises } = data;
 
-    const { routineName, exercises } = req.body;
+  try {
+    // Create a new WorkoutPlan associated with the specific user
+    const newWorkoutPlan = await prisma.workoutPlan.create({
+      data: {
+        name: routineName,
+        userId: userId, // Use the specific user ID
+        exercises: {
+          create: exercises.map((exercise, index) => ({
+            exerciseId: exercise.id,
+            sets: exercise.sets,
+            reps: exercise.reps,
+            duration: exercise.duration,
+            order: index + 1,
+          })),
+        },
+      },
+    });
 
-    try {
-        // Create a new WorkoutPlan
-        const newWorkoutPlan = await prisma.workoutPlan.create({
-            data: {
-                name: routineName,
-                userId: session.user.id, // Use the user's ID from the session
-                exercises: {
-                    create: exercises.map((exercise, index) => ({
-                        exerciseId: exercise.id,
-                        sets: exercise.sets,
-                        reps: exercise.reps,
-                        duration: exercise.duration,
-                        order: index + 1
-                    }))
-                }
-            }
-        });
-
-        res.status(200).json({ success: true, id: newWorkoutPlan.id });
-    } catch (error) {
-        console.error("Failed to insert routine:", error);
-        res.status(500).json({ success: false, error: error.message });
-    }
+    return new Response(JSON.stringify({ success: true, id: newWorkoutPlan.id }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    console.error("Failed to insert routine:", error);
+    return new Response(JSON.stringify({ success: false, error: error.message }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
 }
