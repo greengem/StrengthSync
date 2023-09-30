598c5ebc2e0b7cbc3495e2bded3322ae8186ef19
Green Gem <chris_waitt@me.com>
2023-09-30 12:20:09 +0100

    ui: Styled routines list

--- app/routines/page.tsx
+++ app/routines/page.tsx
@@ -4,6 +4,12 @@ import Heading from '@/components/Layout/Heading'
 import React, { useState, useEffect } from 'react';
 import { Link } from '@nextui-org/link';
 import NextLink from 'next/link';
+import {Card, CardHeader, CardBody, CardFooter} from "@nextui-org/card";
+import {Button} from "@nextui-org/button";
+import {  Table,  TableHeader,  TableBody,  TableColumn,  TableRow,  TableCell} from "@nextui-org/table";
+import {Image} from "@nextui-org/react";
+import NextImage from "next/image";
+import { IconArrowLeft } from '@tabler/icons-react';
 
 interface ExerciseDetail {
   exercise: {
@@ -49,7 +55,9 @@ export default function RoutinesPage() {
   return (
     <PageContainer>
       <Heading title='Routines' />
-      <Link as={NextLink} color='danger' href='/routines/new'>New Routine</Link>
+      <Link as={NextLink} href='/routines/new' className='mb-5'>
+        <Button color='primary'>New Routine</Button>
+      </Link>
       {
         loading ?
           <div className='pt-20 text-center'>
@@ -59,44 +67,60 @@ export default function RoutinesPage() {
           <div>
             {
               routines.length > 0 ?
-<ul>
+<div>
   {
     routines.map((routine) => (
-      <li key={routine.id} className='flex flex-col justify-between items-start px-4 py-2 my-1 rounded-md'>
-        <div className='mb-2'>
-          <p className='font-semibold mb-1'>{routine.name}</p>
-          <p>Created: {new Date(routine.createdAt).toLocaleString()}</p>
-          <p>Last Updated: {new Date(routine.updatedAt).toLocaleString()}</p>
+      <Card key={routine.id} className='mb-10'>
+            <CardHeader className="flex gap-3">
+        <Image
+          as={NextImage}
+          alt="nextui logo"
+          height={40}
+          radius="sm"
+          src="/icons/barbell.svg"
+          width={40}
+        />
+        <div className="flex flex-col">
+          <p className="text-md">{routine.name}</p>
+          <p className="text-small text-default-500">{new Date(routine.updatedAt).toLocaleString()}</p>
         </div>
-        <div>
-          <p className='font-semibold'>Exercises:</p>
-          <ul>
+      </CardHeader>
+        <CardBody>
+          <Table aria-label="Example static collection table">
+          <TableHeader>
+        <TableColumn>Exercise</TableColumn>
+        <TableColumn>Sets</TableColumn>
+        <TableColumn>Reps</TableColumn>
+        <TableColumn>Time</TableColumn>
+      </TableHeader>
+      <TableBody>
             {
-              routine.exercises.map((exerciseDetail, index) => (
-                <li key={exerciseDetail.exercise.id}>
-                  <span className='font-medium'>{index + 1}. {exerciseDetail.exercise.name}</span>
-                  <p>Sets: {exerciseDetail.sets}</p>
-                  <p>Reps: {exerciseDetail.reps}</p>
-                  <p>Duration: {exerciseDetail.duration} seconds</p>
-                </li>
+              routine.exercises.map((exerciseDetail) => (
+                <TableRow key={exerciseDetail.exercise.id}>
+                  <TableCell>{exerciseDetail.exercise.name}</TableCell>
+                  <TableCell>{exerciseDetail.sets}</TableCell>
+                  <TableCell>{exerciseDetail.reps}</TableCell>
+                  <TableCell>{exerciseDetail.duration} seconds</TableCell>
+                  </TableRow>
               ))
             }
-          </ul>
-        </div>
-        <button className="btn btn-primary mt-2">Edit</button>
-      </li>
+            </TableBody>
+          </Table>
+        </CardBody>
+        <CardFooter className='justify-between'>
+          <Link as={NextLink} href='#' className='mr-2'><Button color='secondary'>Edit</Button></Link>
+          <Link as={NextLink} href='#'><Button color='danger'>Delete</Button></Link>
+        </CardFooter>
+      </Card>
     ))
   }
-</ul>
+</div>
 
               :
               <p>
                 No data available
               </p>
             }
-            <div className="mt-10 text-xs ">
-              data received from Vercel Postgres
-            </div>
           </div>
       }
     </PageContainer>
